b91ebeab7ee22b78c08304e4e923dbdc045999c6
Anurag Sahu <as3305100@gmail.com>
2025-10-15 22:30:25 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import configuration from "./config/configuration";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    AuthModule,
+  ],
+})
+export class AppModule {}
